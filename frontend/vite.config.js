ad425e103cf2fce22d5555c84017e5654dd6543b
Mohini Deshmukh <mohinideshmukh786@gmail.com>
2026-05-04 16:52:39 +0530

    edited vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+
+
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name]-[hash]-v2.js`, // Adding -v2 forces a fresh file
+      },
+    },
+  },
 })
